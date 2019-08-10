aee7751042843a774f58ea6c3b5f9e0832343488
Michael Liendo <mtliendo@gmail.com>
2019-08-10 13:25:08 -0500

    add initial query to frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import { getGifs } from "./graphql/queries";
 
-function App() {
+const App = () => {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    API.graphql(
+      graphqlOperation(getGifs, { searchTerm: "got", limit: 2 })
+    ).then(data => console.log(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1>Hello!</h1>
+    </header>
   );
-}
+};
 
 export default App;
